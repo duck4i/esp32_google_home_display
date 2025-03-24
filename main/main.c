d90fdc56cabe41f1f72a920185154a0c3a61a069
duck4i <mihailogazda@gmail.com>
2025-03-24 10:14:47 +0000

    initial commit and build

---
+++ main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+const char *TAG = "GHOME";
+
+void ghome_task(void *pvParameter)
+{
+    while (1)
+    {
+        ESP_LOGI(TAG, "GHOME task is running");
+        vTaskDelay(pdMS_TO_TICKS(250));
+    }
+}
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Starting GHOME systems");
+
+    xTaskCreate(ghome_task, "ghome_task", 4096, NULL, 5, NULL);
+}
