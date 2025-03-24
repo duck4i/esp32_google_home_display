f8a7e0b723b888561e227d7c45f4e75e8805d8b1
duck4i <mihailogazda@gmail.com>
2025-03-24 16:05:12 +0000

    display device cleaned up

---
+++ main/display_device.cpp
@@ -0,0 +1,101 @@
+#include "display_device.h"
+
+#ifdef LCD_TYPE_ST7789_SPI
+#define LCD_MOSI 23
+#define LCD_SCLK 18
+#define LCD_BLK 32
+#define LCD_CS 15
+#define LCD_DC 2
+#define LCD_RST 4
+#endif
+
+DisplayDevice::DisplayDevice(uint16_t width, uint16_t height)
+{
+#if defined(LCD_TYPE_ST7789_SPI)
+
+    //  Configure bus
+    auto bcfg = m_bus.config();
+
+    // SPI bus pins
+    bcfg.spi_host = HSPI_HOST;
+    bcfg.spi_mode = 0;
+    // bcfg.freq_write = 40000000; // 40MHz
+    // bcfg.freq_read = 16000000;  // 16MHz
+    bcfg.spi_3wire = false;
+    bcfg.use_lock = true;
+    bcfg.dma_channel = 1;
+
+    // Pin assignment
+    bcfg.pin_mosi = LCD_MOSI;
+    bcfg.pin_miso = -1; // Not used
+    bcfg.pin_sclk = LCD_SCLK;
+    bcfg.pin_dc = LCD_DC;
+
+    m_bus.config(bcfg);
+    m_panel.setBus(&m_bus);
+
+    //  Configure the panel
+    auto cfg = m_panel.config();
+
+    cfg.pin_cs = LCD_CS;
+    cfg.pin_rst = LCD_RST;
+    cfg.pin_busy = -1;
+
+    cfg.panel_width = width;
+    cfg.panel_height = height;
+    cfg.offset_x = 52;
+    cfg.offset_y = 40;
+    // cfg.offset_rotation = 1;
+    cfg.readable = false;
+    cfg.invert = true;
+    cfg.rgb_order = false;
+    cfg.dlen_16bit = false;
+    cfg.bus_shared = false;
+
+    m_panel.config(cfg);
+    setPanel(&m_panel);
+
+#elif defined(LCD_TYPE_ST7789_8BIT)
+    auto bcfg = m_bus.config();
+    bcfg.pin_wr = 4;
+    bcfg.pin_rd = 2;
+    bcfg.pin_rs = 16;
+
+    bcfg.pin_d0 = 15;
+    bcfg.pin_d1 = 13;
+    bcfg.pin_d2 = 12;
+    bcfg.pin_d3 = 14;
+    bcfg.pin_d4 = 27;
+    bcfg.pin_d5 = 25;
+    bcfg.pin_d6 = 33;
+    bcfg.pin_d7 = 32;
+
+    m_bus.config(bcfg);
+    m_panel.setBus(&m_bus);
+
+    auto cfg = m_panel.config();
+
+    cfg.pin_cs = 17;
+    cfg.pin_rst = -1;
+    cfg.pin_busy = -1;
+
+    cfg.panel_width = width;
+    cfg.panel_height = height;
+    cfg.offset_x = 0;
+    cfg.offset_y = 0;
+    cfg.offset_rotation = 0;
+    // cfg.dummy_read_pixel = 8;
+    // cfg.dummy_read_bits = 1;
+    cfg.readable = false;
+    cfg.invert = false;
+    cfg.rgb_order = false;
+    cfg.dlen_16bit = false;
+    cfg.bus_shared = false;
+
+    m_panel.config(cfg);
+    setPanel(&m_panel);
+
+#else
+#error "No panel selected"
+#endif
+}
--- main/render.cpp
+++ main/render.cpp
@@ -1,109 +1,25 @@
 #include "render.h"
 #include <esp_log.h>
 #include <lvgl.h>
-#include "lgfx/v1/LGFXBase.hpp"
-#include "lgfx/v1/panel/Panel_ST7789.hpp"
-#include "lgfx/v1/platforms/device.hpp"
+#include "display_device.h"
 #include <debounced_button.hpp>
-
-// #include <demos/lv_demos.h>
 
 #define TAG "GHOME_RENDER"
 
 #define LCD_WIDTH 135
 #define LCD_HEIGHT 240
 
-#define LCD_MOSI 23
-#define LCD_SCLK 18
-#define LCD_BLK 32
-#define LCD_CS 15
-#define LCD_DC 2
-#define LCD_RST 4
-
-class Device : public lgfx::LGFX_Device
-{
-private:
-    lgfx::Panel_ST7789 m_panel;
-    lgfx::Bus_SPI m_bus;
-
-public:
-    Device()
-    {
-        //  Configure bus
-        auto bcfg = m_bus.config();
-
-        // SPI bus pins
-        bcfg.spi_host = HSPI_HOST;
-        bcfg.spi_mode = 0;
-        // bcfg.freq_write = 40000000; // 40MHz
-        // bcfg.freq_read = 16000000;  // 16MHz
-        bcfg.spi_3wire = false;
-        bcfg.use_lock = true;
-        bcfg.dma_channel = 1;
-
-        // Pin assignment
-        bcfg.pin_mosi = LCD_MOSI;
-        bcfg.pin_miso = -1; // Not used
-        bcfg.pin_sclk = LCD_SCLK;
-        bcfg.pin_dc = LCD_DC;
-
-        m_bus.config(bcfg);
-        m_panel.setBus(&m_bus);
-
-        //  Configure the panel
-        auto cfg = m_panel.config();
-
-        cfg.pin_cs = LCD_CS;
-        cfg.pin_rst = LCD_RST;
-        cfg.pin_busy = -1;
-
-        cfg.panel_width = LCD_WIDTH;
-        cfg.panel_height = LCD_HEIGHT;
-        cfg.offset_x = 52;
-        cfg.offset_y = 40;
-        // cfg.offset_rotation = 1;
-        cfg.readable = false;
-        cfg.invert = true;
-        cfg.rgb_order = false;
-        cfg.dlen_16bit = false;
-        cfg.bus_shared = false;
-
-        m_panel.config(cfg);
-
-        setPanel(&m_panel);
-    }
-};
-
-Device lcd = Device();
-
 #define DRAW_BUF_SIZE (LCD_WIDTH * LCD_HEIGHT * (LV_COLOR_DEPTH / 8))
 DRAM_ATTR WORD_ALIGNED_ATTR uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-static lv_display_t *LVGL_display;
-static lv_indev_t *LVGL_input_device;
-
-FORCE_INLINE_ATTR uint32_t lv_tick(void)
+struct devices
 {
-    return esp_timer_get_time() / 1000;
-}
+    DisplayDevice lcd{LCD_WIDTH, LCD_HEIGHT};
+    lv_display_t *display{nullptr};
+    lv_indev_t *input{nullptr};
+};
 
-FORCE_INLINE_ATTR void lv_disp_flush(lv_display_t *display, const lv_area_t *area, uint8_t *px_map)
-{
-    auto w = (area->x2 - area->x1 + 1);
-    auto h = (area->y2 - area->y1 + 1);
-    uint32_t size = w * h;
-
-    lcd.startWrite();                            /* Start new TFT transaction */
-    lcd.setAddrWindow(area->x1, area->y1, w, h); /* set the working window */
-    lcd.writePixels((uint16_t *)px_map, size, true);
-    lcd.endWrite(); /* terminate TFT transaction */
-
-    lv_display_flush_ready(display); /* tell lvgl that flushing is done */
-}
-
-FORCE_INLINE_ATTR void lv_input_read(lv_indev_t *indev, lv_indev_data_t *data)
-{
-}
+devices dev{};
 
 struct components
 {
@@ -118,6 +34,29 @@ struct components
 };
 
 components ui{};
+
+FORCE_INLINE_ATTR uint32_t lv_tick(void)
+{
+    return esp_timer_get_time() / 1000;
+}
+
+FORCE_INLINE_ATTR void lv_disp_flush(lv_display_t *display, const lv_area_t *area, uint8_t *px_map)
+{
+    auto w = (area->x2 - area->x1 + 1);
+    auto h = (area->y2 - area->y1 + 1);
+    uint32_t size = w * h;
+
+    dev.lcd.startWrite();                            /* Start new TFT transaction */
+    dev.lcd.setAddrWindow(area->x1, area->y1, w, h); /* set the working window */
+    dev.lcd.writePixels((uint16_t *)px_map, size, true);
+    dev.lcd.endWrite(); /* terminate TFT transaction */
+
+    lv_display_flush_ready(display); /* tell lvgl that flushing is done */
+}
+
+FORCE_INLINE_ATTR void lv_input_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+}
 
 void lv_setup_components()
 {
@@ -145,13 +84,8 @@ void ghome_render_init()
 {
     ESP_LOGI(TAG, "GHOME render init");
 
-    lcd.init();
-    lcd.fillScreen(TFT_ORANGE);
-
-    lcd.setTextSize(1);
-    lcd.setTextColor(TFT_YELLOW);
-    lcd.setFont(&lgfx::fonts::FreeMono9pt7b);
-    lcd.setRotation(0);
+    dev.lcd.init();
+    dev.lcd.fillScreen(TFT_BLACK);
 
     ESP_LOGI(TAG, "PANEL_INIT_DONE");
 
@@ -172,24 +106,23 @@ void ghome_render_init()
     lv_init();
     lv_tick_set_cb(lv_tick);
 
-    LVGL_display = lv_display_create(LCD_WIDTH, LCD_HEIGHT);
-    lv_display_set_flush_cb(LVGL_display, lv_disp_flush);
+    dev.display = lv_display_create(LCD_WIDTH, LCD_HEIGHT);
+    lv_display_set_flush_cb(dev.display, lv_disp_flush);
     ESP_LOGI(TAG, "LVGL_DISPLAY_DONE");
 
-    lv_display_set_buffers(LVGL_display, draw_buf, NULL, sizeof(draw_buf), LV_DISP_RENDER_MODE_PARTIAL);
-    lv_display_set_antialiasing(LVGL_display, false);
-    // lv_display_set_color_format(LVGL_display, LV_COLOR_FORMAT_RGB565);
+    lv_display_set_buffers(dev.display, draw_buf, NULL, sizeof(draw_buf), LV_DISP_RENDER_MODE_PARTIAL);
+    lv_display_set_antialiasing(dev.display, false);
+    lv_display_set_color_format(dev.display, LV_COLOR_FORMAT_RGB565);
+
     ESP_LOGI(TAG, "LVGL_DISPLAY_BUFF_DONE");
 
-    LVGL_input_device = lv_indev_create();
-    lv_indev_set_type(LVGL_input_device, LV_INDEV_TYPE_NONE);
-    lv_indev_set_read_cb(LVGL_input_device, lv_input_read);
+    dev.input = lv_indev_create();
+    lv_indev_set_type(dev.input, LV_INDEV_TYPE_NONE);
+    lv_indev_set_read_cb(dev.input, lv_input_read);
     ESP_LOGI(TAG, "LVGL_INPUT_INIT_DONE");
 
     ESP_LOGI(TAG, "LVGL_INIT_DONE");
 
-    // lv_demo_widgets();
-    // lv_demo_widgets_start_slideshow();
     lv_setup_components();
 }
 
